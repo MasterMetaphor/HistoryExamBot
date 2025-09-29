acab539ef1cb8431cb6c2d4c499ba1c83cf95a91
MasterMetaphor <masterofmetaphors@yahoo.com>
2025-09-28 20:19:34 -0500

    Delete docs directory

--- docs/start_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting History 2310 Study Bot Server...
-echo.
-echo The server will be available at: http://localhost:8000
-echo Press Ctrl+C to stop the server
-echo.
-python server.py
-pause
